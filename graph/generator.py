03682e4ee72f0354385a9ece8c3484be7a3cfd5f
Горлов Максим <gorlov.maxim@gmail.com>
2021-02-07 23:58:45 +0300

    now with techs

--- graph/generator.py
+++ graph/generator.py
@@ -5,6 +5,7 @@ import json
 import requests
 import os.path
 import sys
+from wand.image import Image
 
 unknown_icon = "https://dsp-wiki.com/images/1/10/Icon_Unknown.png"
 icon_size = 64
@@ -27,13 +28,22 @@ class CGenerator:
             if not block:
                 break
             icon_file.write(block)
-    return icon_filename
 
-  def get_icon_filename(self, group, key):
+  def get_icon_filename(self, group, key, size):
+    icon_filename = 'icons/{0}_{1}.png'.format(group, key)
+    sized_icon_filename = 'icons/{0}_{1}_{2}.png'.format(group, key, size)
     if key in self.data[group]:
       item = self.data[group][key]
-      return self.download_icon(item["icon"], 'icons/{0}_{1}.png'.format(group, key))
-    return self.download_icon(unknown_icon, 'icons/unknown.png')
+      self.download_icon(item["icon"], icon_filename)
+    else:
+      icon_filename = 'icons/unknown.png'.format(group, key)
+      sized_icon_filename = 'icons/unknown_{0}.png'.format(size)
+      self.download_icon(unknown_icon, icon_filename)
+    if not os.path.isfile(sized_icon_filename):
+      with Image(filename=icon_filename) as img:
+        img.transform(resize='{0}x{0}>'.format(size))
+        img.save(filename=sized_icon_filename)
+    return sized_icon_filename
 
   def get_color(self, group, name):
     # print(name)
@@ -45,7 +55,7 @@ class CGenerator:
       for flag in item["flags"]:
         flags += " {0} ".format(self.data["icons"][flag])
       return '<tr><td colspan="2">{0}</td></tr>'.format(flags)
-    return ""
+    return ''
 
   def count_usage(self, target_item_key):
     count = 0
@@ -58,6 +68,13 @@ class CGenerator:
               count+=1
     return count
 
+  def get_item_tech(self, item_key):
+    for tech_key in self.data["technologies"]:
+      tech = self.data["technologies"][tech_key]
+      if item_key in tech["unlocks"]:
+        return '<tr><td colspan="2">{0}</td></tr><tr><td colspan="2"><img src="{1}" /></td></tr>'.format(tech["name"], self.get_icon_filename("technologies", tech_key, 32))
+    return ''
+
   def generate_items_nodes(self, gv_file):
     for item_key in self.data["items"]:
       print("Generating node {}".format(item_key))
@@ -69,20 +86,22 @@ class CGenerator:
                 <td colspan="2">{1}</td>
               </tr>
               <tr>
-                <td fixedsize="true" width="{6}" height="{6}"><img src='{2}' /></td>
+                <td><img src='{2}' /></td>
                 <td>Usages: {5}</td>
               </tr>
+              {6}
               {4}
              </table>
              >, fillcolor="#{3}"];
 """.format(
-          item_key,
-          item["name"],
-          self.get_icon_filename("items", item_key),
-          self.get_color("nodes", item["type"]),
-          self.get_flags_icons(item),
-          self.count_usage(item_key),
-          icon_size))
+            item_key,
+            item["name"],
+            self.get_icon_filename("items", item_key, 64),
+            self.get_color("nodes", item["type"]),
+            self.get_flags_icons(item),
+            self.count_usage(item_key),
+            self.get_item_tech(item_key)
+          ))
 
   def generate_items_edges(self, gv_file):
     for item_key in self.data["items"]:
@@ -98,41 +117,59 @@ class CGenerator:
                 recipe_item,
                 self.get_color("edges", recipe["assembler"])))
 
-  def generate_options(self, gv_file, splines, ratio, minlen):
-      gv_file.write('  graph [overlap=false, splines={2}, ratio={3}, bgcolor="#{0}", fontcolor="#{1}", fillcolor="#{0}", fontname=Roboto, fontsize=10];\n'.format(
+  def generate_options(self, gv_file, splines, ratio, minlen, rankdir):
+      gv_file.write('  graph [overlap=false, splines={2}, ratio={3}, bgcolor="#{0}", fontcolor="#{1}", fillcolor="#{0}", rankdir="{4}" fontname=Roboto, fontsize=10];\n'.format(
         self.get_color("main", "background"),
         self.get_color("main", "font"),
         splines,
-        ratio))
+        ratio,
+        rankdir))
       gv_file.write('  node [shape=box, style="rounded,filled", fontcolor="#{0}", color="#{0}"];\n'.format(
             self.get_color("main", "font")))
       gv_file.write('  edge [fontcolor="#{0}", color="#{0}", penwidth=3, minlen={1}];\n'.format(
             self.get_color("main", "font"),
             minlen))
 
+  def get_tech_res_gv_html(self, tech, group):
+    icons = []
+    for item in tech[group]:
+      icons.append("<td><img src='{0}' /></td>".format(self.get_icon_filename("items", item, 32)))
+    result = ""
+    if len(icons) > 0:
+      result = '<tr><td>{0}</td></tr><tr><td><table border="0" cellborder="0" cellspacing="0"><tr>{1}</tr></table></td></tr>'.format(group.title(), "".join(icons))
+    return result
+
   def generate_tech_nodes(self, gv_file):
     self.used_types_keys.append("tech")
     for tech_key in self.data["technologies"]:
       print("Generating node {}".format(tech_key))
       tech = self.data["technologies"][tech_key]
+
       gv_file.write("""  tech_{0} [label=<
              <table border="0" cellborder="0" cellspacing="0">
               <tr>
                 <td>{1}</td>
               </tr>
               <tr>
-                <td fixedsize="true" width="{4}" height="{4}"><img src='{2}' /></td>
+                <td><img src='{2}' /></td>
               </tr>
+              {4}
+              {5}
              </table>
              >, fillcolor="#{3}"];
 """.format(
           tech_key,
           tech["name"],
-          self.get_icon_filename("technologies", tech_key),
+          self.get_icon_filename("technologies", tech_key, 64),
           self.get_color("nodes", "tech"),
-          icon_size))
+          self.get_tech_res_gv_html(tech, "unlocks"),
+          self.get_tech_res_gv_html(tech, "resources")))
 
   def generate_tech_edges(self, gv_file):
+    self.used_assemblers_keys.append("tech")
+    if "items" in self.graphs_list:
+      self.used_assemblers_keys.append("unlocks")
+      self.used_assemblers_keys.append("resources")
     for tech_key in self.data["technologies"]:
       print("Generating edge {}".format(tech_key))
       tech = self.data["technologies"][tech_key]
@@ -157,27 +194,27 @@ class CGenerator:
     self.graphs_list = graphs_list
     self.used_types_keys = []
     self.used_assemblers_keys = []
-    with open('graph_{0}.gv'.format('_'.join(self.graphs_list)), "w") as gv_file:
+    with open('result/graph_{0}.gv'.format('_'.join(self.graphs_list)), "w") as gv_file:
       gv_file.write("digraph g {\n")
-      self.generate_options(gv_file, "ortho", 9/16, 4)
+      self.generate_options(gv_file, "ortho", 9/16, 4 if "items" in self.graphs_list else 1, "TB" if "items" in self.graphs_list else "LR")
       if "items" in self.graphs_list:
         self.generate_items_nodes(gv_file)
         self.generate_items_edges(gv_file)
       if "tech" in self.graphs_list:
         self.generate_tech_nodes(gv_file)
         self.generate_tech_edges(gv_file)
-      gv_file.write('subgraph cluster_0 {{ label="Legend"; legend [image="legend_{1}.{0}", label="", shape="none"]; }}\n'.format(sys.argv[1], '_'.join(self.graphs_list)))
+      gv_file.write('subgraph cluster_0 {{ label="Legend"; legend [image="result/legend_{1}.{0}", label="", shape="none"]; }}\n'.format(sys.argv[1], '_'.join(self.graphs_list)))
       gv_file.write("}\n")
     self.generate_legend()
 
   def generate_legend(self):
-    with open('legend_{0}.gv'.format('_'.join(self.graphs_list)), "w") as gv_file:
+    with open('result/legend_{0}.gv'.format('_'.join(self.graphs_list)), "w") as gv_file:
       gv_file.write('digraph g {\n')
-      self.generate_options(gv_file, "ortho", "auto", 1)
+      self.generate_options(gv_file, "ortho", "auto", 1, "TB")
+      gv_file.write('types [label="Types", fillcolor="#{0}"]; assemblers [label="Relations", fillcolor="#{0}"]; flags [label="Flags", fillcolor="#{0}"]; '.format(
+            self.get_color("main", "background")
+          ))
       if "items" in self.graphs_list:
-        gv_file.write('types [label="Types", fillcolor="#{0}"]; assemblers [label="Assemblers", fillcolor="#{0}"]; flags [label="Flags", fillcolor="#{0}"]; '.format(
-              self.get_color("main", "background")
-            ))
         for icon_key in self.data["legend"]["icons"]:
           gv_file.write('  {0} [label="{1} {2}", fillcolor="#{3}"]; flags ->{0} [penwidth=1];\n'.format(
                 icon_key,
@@ -186,23 +223,19 @@ class CGenerator:
                 self.get_color("main", "background")
               ))
       used_nodes_keys = set(self.used_types_keys)
-      print(used_nodes_keys)
       for node_color_key in self.data["legend"]["colors"]["nodes"]:
-        print(node_color_key)
         if node_color_key in used_nodes_keys:
           gv_file.write('  {0} [label="{1}", fillcolor="#{2}"];\n'.format(
                 node_color_key,
                 self.data["legend"]["colors"]["nodes"][node_color_key],
                 self.get_color("nodes", node_color_key)
               ))
-          if "items" in self.graphs_list:
-            gv_file.write('  types -> {0} [penwidth=1];\n'.format(
-                  node_color_key,
-                  self.data["legend"]["colors"]["nodes"][node_color_key],
-                  self.get_color("nodes", node_color_key)
-                ))
+          gv_file.write('  types -> {0} [penwidth=1];\n'.format(
+                node_color_key,
+                self.data["legend"]["colors"]["nodes"][node_color_key],
+                self.get_color("nodes", node_color_key)
+              ))
       used_edges_keys = set(self.used_assemblers_keys)
-      print(used_edges_keys)
       for edge_color_key in self.data["legend"]["colors"]["edges"]:
         if edge_color_key in used_edges_keys:
           gv_file.write('  {0}_a [label="{1}", fillcolor="#{2}"]; {0}_b [shape=point]; assemblers -> {0}_a [penwidth=1]; {0}_a -> {0}_b [color="#{3}"]; \n'.format(
